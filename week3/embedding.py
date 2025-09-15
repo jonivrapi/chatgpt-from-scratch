9dd0405b7ee75a4ad18fa06278b8d2da3b860490
Joni Vrapi <jonivrapi@yahoo.com>
2025-09-15 19:05:24 -0400

    WIP

--- week3/embedding.py
+++ week3/embedding.py
@@ -5,12 +5,12 @@ Complete this class by instantiating a parameter called "self.weight", and
 use it to complete the forward() method. You do not need to worry about backpropogation.
 '''
 class CustomEmbedding(torch.nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int):
+        super().__init__()
+        self.weight = torch.nn.Parameter(
+            # should be a random uniform distribution between -1 and 1
+            torch.rand(num_embeddings, embedding_dim) * 2 - 1
+        )
 
-	def __init__(self, num_embeddings, embedding_dim):
-		super().__init__()
-		# TODO
-
-	def forward(self, x):
-		# x is a tensor of integers
-		# TODO
-		
+    def forward(self, x: torch.Tensor):
+        return self.weight[x.long()]
--- week3/tokenizer.py
+++ week3/tokenizer.py
@@ -27,37 +27,76 @@ For example, if your merges contain: ("m","o"), ("s","e"), ("u","s"), then
 
 '''
 
+import json
+
 class Tokenizer:
-    
-    def __init__(self, vocab_file, merges_file):
-        # TODO
+    def __init__(self, vocab_file: str, merges_file: str):
+        # Load vocabulary
+        with open(vocab_file, "r", encoding="utf-8") as f:
+            self.id_to_token = [line.rstrip("\n") for line in f]
+        self.token_to_id = {t: i for i, t in enumerate(self.id_to_token)}
 
+        # Load merges
+        with open(merges_file, "r", encoding="utf-8") as f:
+            merges = json.load(f)
+        # Give each pair a rank where earlier = higher priority
+        self.rank = {(a, b): i for i, (a, b) in enumerate(merges)}
 
-    def encode(self, string):
-        '''
-        param string : a string to be encoded
-        returns a list of integers (token ids)
-        '''
+    def encode(self, text: str):
+        tokens = list(text)
 
-        # TODO
+        # Repeatedly apply the best-ranked merge present
+        while True:
+            pairs = [(tokens[i], tokens[i + 1]) for i in range(len(tokens) - 1)]
 
+            best = None
+            best_rank = None
+            for p in pairs:
+                rank = self.rank.get(p)
+                rank_exists = rank is not None
+                if rank_exists:
+                    rank_is_better = (best_rank is None) or (rank < best_rank)
+                    if rank_is_better:
+                        best, best_rank = p, rank
 
-    def decode(self, list_of_integers):
-        '''
-        param list_of_integers : a list of token ids
-        returns a string formed by decoding these ids.
-        '''
+            no_more_merges = best is None
+            if no_more_merges:
+                break
 
-        # TODO
+            # Merge all occurrences of the best pair, left-to-right
+            merged = []
+            i = 0
+            n = len(tokens)
+            while i < n:
+                has_next = i < n - 1
+                next_pair = (tokens[i], tokens[i + 1]) if has_next else None
+                next_pair_is_best = has_next and (next_pair == best)
+
+                if next_pair_is_best:
+                    merged.append(tokens[i] + tokens[i + 1])
+                    i += 2
+                else:
+                    merged.append(tokens[i])
+                    i += 1
+
+            tokens = merged
+
+        # Map final tokens to ids
+        ids = []
+        for t in tokens:
+            ids.append(self.token_to_id[t])
+        return ids
+
+    def decode(self, ids):
+        return "".join(self.id_to_token[i] for i in ids)
 
 
 
 if __name__ == "__main__":
-
-    # example of using this class
-
     tok = Tokenizer("./vocab.txt", "./merges.json")
-    x = tok.encode("Peter piper picked a peck of pickled peppers.")
-    print(x)
-    x = tok.decode(x)
-    print(x) # should be our original text.
+    s = "Peter piper picked a peck of pickled peppers."
+    ids = tok.encode(s)
+    print(ids)
+    print(tok.decode(ids))  # should equal s
+    print(s == tok.decode(ids))
+
